f8a19a5f5b80b3c0c62758559842a910523e67f8
Ce Fang <cfan0025@student.monash.edu>
2021-06-03 21:49:23 +1000

    add delay to animation

--- script/bar_race.js
+++ script/bar_race.js
@@ -1,12 +1,12 @@
-var margin = { top: 20, right: 50, bottom: 30, left: 50 },
-    width = 800 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+var b_margin = { top: 20, right: 50, bottom: 30, left: 50 },
+    b_width = 960 - margin.left - margin.right,
+    b_height = 300 - margin.top - margin.bottom;
 var bar_svg = d3.select("#bar_chart")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", b_width + b_margin.left + b_margin.right)
+    .attr("height", b_height + b_margin.top + b_margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(10," + b_margin.top + ")");
 
 var color = d3.scale.ordinal();
 
@@ -39,29 +39,27 @@ d3.csv("/data/bar.csv", function (data) {
     var dateSlice = data.filter(function (d) { return d.acq_date == time })
         .slice(0, 3);
 
-    console.log(dateSlice);
-
     var barX = d3.scale.linear()
         .domain([0, d3.max(dateSlice, function (d) { return d.cumu_sum })])
-        .range([0, width]);
+        .range([0, b_width]);
 
     var barY = d3.scale.linear()
         .domain([3, 0])
-        .range([height, 0]);
+        .range([b_height, 0]);
 
     var xAx = d3.svg.axis()
         .scale(barX)
         .ticks(width > 500 ? 5 : 2)
-        .innerTickSize(-(height))
+        .innerTickSize(-(b_height))
         .orient("top");
 
     bar_svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + margin.top + ")")
+        .attr("transform", "translate(0," + b_margin.top + ")")
         .call(xAx);
 
     bar_svg.selectAll("rect")
-        .data(dateSlice, function(d){return d.state})
+        .data(dateSlice, function (d) { return d.state })
         .enter()
         .append("rect")
         .attr("class", "bar")
@@ -92,8 +90,8 @@ d3.csv("/data/bar.csv", function (data) {
 
     var timeStamp = bar_svg.append("text")
         .attr("class", "bar_time")
-        .attr("x", width + margin.right)
-        .attr("y", height + 20)
+        .attr("x", b_width + b_margin.right)
+        .attr("y", b_height + 20)
         .style({
             "text-anchor": "end",
             "stroke-width": 3,
@@ -105,124 +103,129 @@ d3.csv("/data/bar.csv", function (data) {
         })
         .html(time.substring(0, 7));
 
-    let ticker = setInterval(function (e) {
-        dateSlice = data.filter(function (d) { return d.acq_date == time })
-            .slice(0, 3);
+    var delayTime = 1000;
 
-        console.log(dateSlice);
+    //set delay
+    setTimeout(function (d) {
+        let ticker = setInterval(function (e) {
+            dateSlice = data.filter(function (d) { return d.acq_date == time })
+                .slice(0, 3);
 
-        barX.domain([0, d3.max(dateSlice, function (d) { return d.cumu_sum })]);
+            barX.domain([0, d3.max(dateSlice, function (d) { return d.cumu_sum })]);
 
-        bar_svg.select(".axis")
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .call(xAx);
+            bar_svg.select(".axis")
+                .transition()
+                .duration(duration)
+                .ease("linear")
+                .call(xAx);
 
-        var bars = bar_svg.selectAll(".bar").data(dateSlice, function (d) { return d.state });
+            var bars = bar_svg.selectAll(".bar").data(dateSlice, function (d) { return d.state });
 
-        bars
-            .enter()
-            .append("rect")
-            .attr("class", "bar")
-            .attr("x", barX(0))
-            .attr("width", function (d) { return barX(d.cumu_sum) - barX(0) - 1 })
-            .attr("y", barY(4) - 30)
-            .attr("height", 20)
-            .style("fill", function (d) { return color(d.state); })
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .attr("y", function (d) { return barY(d.rank) - 30 });
+            bars
+                .enter()
+                .append("rect")
+                .attr("class", "bar")
+                .attr("x", barX(0))
+                .attr("width", function (d) { return barX(d.cumu_sum) - barX(0) - 1 })
+                .attr("y", barY(4) - 30)
+                .attr("height", 20)
+                .style("fill", function (d) { return color(d.state); })
+                .transition()
+                .duration(duration)
+                .ease("linear")
+                .attr("y", function (d) { return barY(d.rank) - 30 });
 
-        bars.transition()
-            .duration(duration)
-            .ease("linear")
-            .attr("width", function (d) { return barX(d.cumu_sum) - barX(0) - 1 })
-            .attr("y", function (d) { return barY(d.rank) - 30 });
-        
-        bars
-            .exit()
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .attr("width", function (d) { return barX(d.cumu_sum) - barX(0) - 1 })
-            .attr("y", barY(4) - 30)
-            .remove();
+            bars.transition()
+                .duration(duration)
+                .ease("linear")
+                .attr("width", function (d) { return barX(d.cumu_sum) - barX(0) - 1 })
+                .attr("y", function (d) { return barY(d.rank) - 30 });
 
-        var labels = bar_svg.selectAll(".bar_label")
-            .data(dateSlice, function (d) { return d.state });
+            bars
+                .exit()
+                .transition()
+                .duration(duration)
+                .ease("linear")
+                .attr("width", function (d) { return barX(d.cumu_sum) - barX(0) - 1 })
+                .attr("y", barY(4) - 30)
+                .remove();
 
-        labels
-            .enter()
-            .append("text")
-            .attr("class", "bar_label")
-            .attr("x", function (d) { return barX(d.cumu_sum) - 8 })
-            .attr("y", function (d) { return barY(4) - 15 })
-            .attr("text-anchor", "end")
-            .html(function (d) { return d.state })
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .attr("y", function (d) { return barY(d.rank) - 15 });
+            var labels = bar_svg.selectAll(".bar_label")
+                .data(dateSlice, function (d) { return d.state });
 
-        labels
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .attr("x", function (d) { return barX(d.cumu_sum) - 8 })
-            .attr("y", function (d) { return barY(d.rank) - 15 });
+            labels
+                .enter()
+                .append("text")
+                .attr("class", "bar_label")
+                .attr("x", function (d) { return barX(d.cumu_sum) - 8 })
+                .attr("y", function (d) { return barY(4) - 15 })
+                .attr("text-anchor", "end")
+                .html(function (d) { return d.state })
+                .transition()
+                .duration(duration)
+                .ease("linear")
+                .attr("y", function (d) { return barY(d.rank) - 15 });
 
-        labels
-            .exit()
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .attr("x", function (d) { return barX(d.cumu_sum) - 8 })
-            .attr("y", function (d) { return barY(4) - 15 })
-            .remove();
+            labels
+                .transition()
+                .duration(duration)
+                .ease("linear")
+                .attr("x", function (d) { return barX(d.cumu_sum) - 8 })
+                .attr("y", function (d) { return barY(d.rank) - 15 });
 
-        var val_labels = bar_svg.selectAll(".bar_val").data(dateSlice, function (d) { return d.state });
+            labels
+                .exit()
+                .transition()
+                .duration(duration)
+                .ease("linear")
+                .attr("x", function (d) { return barX(d.cumu_sum) - 8 })
+                .attr("y", function (d) { return barY(4) - 15 })
+                .remove();
 
-        val_labels
-            .enter()
-            .append("text")
-            .attr("class", "bar_val")
-            .attr("x", function (d) { return barX(d.cumu_sum) + 8 })
-            .attr("y", function (d) { return barY(4) - 15 })
-            .text(function (d) { return d.cumu_sum })
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .attr("y", function (d) { return barY(d.rank) - 15 });
+            var val_labels = bar_svg.selectAll(".bar_val").data(dateSlice, function (d) { return d.state });
 
-        val_labels
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .attr("x", function (d) { return barX(d.cumu_sum) + 8 })
-            .attr("y", function (d) { return barY(d.rank) - 15 })
-            .tween("text", function (d) {
-                var i = d3.interpolateRound(d.cumu_sum, d.next_val);
-                return function (t) {
-                    this.textContent = d3.format(",")(i(t));
-                }
-            });
+            val_labels
+                .enter()
+                .append("text")
+                .attr("class", "bar_val")
+                .attr("x", function (d) { return barX(d.cumu_sum) + 8 })
+                .attr("y", function (d) { return barY(4) - 15 })
+                .text(function (d) { return d.cumu_sum })
+                .transition()
+                .duration(duration)
+                .ease("linear")
+                .attr("y", function (d) { return barY(d.rank) - 15 });
 
-        val_labels
-            .exit()
-            .transition()
-            .duration(duration)
-            .ease("linear")
-            .attr("x", function (d) { return barX(d.cumu_sum) + 8 })
-            .attr("y", function (d) { return barY(4) - 15 })
-            .remove();
-        timeStamp.html(time.substring(0, 7));
+            val_labels
+                .transition()
+                .duration(duration)
+                .ease("linear")
+                .attr("x", function (d) { return barX(d.cumu_sum) + 8 })
+                .attr("y", function (d) { return barY(d.rank) - 15 })
+                .tween("text", function (d) {
+                    var i = d3.interpolateRound(d.cumu_sum, d.next_val);
+                    return function (t) {
+                        this.textContent = d3.format(",")(i(t));
+                    }
+                });
 
-        if (time == "2020-01-10") clearInterval(ticker)
-        var parsed = parseDate(time);
-        parsed = d3.time.day.offset(parsed, 1);
-        time = formatDate(parsed);
-    },duration);
+            val_labels
+                .exit()
+                .transition()
+                .duration(duration)
+                .ease("linear")
+                .attr("x", function (d) { return barX(d.cumu_sum) + 8 })
+                .attr("y", function (d) { return barY(4) - 15 })
+                .remove();
+            timeStamp.html(time.substring(0, 7));
+
+            if (time == "2020-01-10") clearInterval(ticker)
+            var parsed = parseDate(time);
+            parsed = d3.time.day.offset(parsed, 1);
+            time = formatDate(parsed);
+        }, duration);
+    }, delayTime);
+
+
 
 });
